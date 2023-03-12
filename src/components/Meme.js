2d1ff7f460f570fdc31830921e71edf9e50a75d4
Azhar <abdazhar786@gmail.com>
2023-03-12 14:31:06 +0530

    Add meme

---
+++ src/components/Meme.js
@@ -0,0 +1,20 @@
+import React from "react";
+ 
+export default function(){
+    return(
+        <main>
+            <div className="form">
+                <input 
+                type="text" 
+                placeholder="Top text" 
+                className="form--input"/>
+
+                <input 
+                type="text" 
+                placeholder="Bottom text"
+                className="form--input" /> 
+                  <button className="form--button">Get a new meme image  🖼</button>
+            </div>
+        </main>
+    )
+}
